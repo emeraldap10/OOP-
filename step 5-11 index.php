0bd929666e29cb41a372f2f36011c2e26550bec8
emeraldap10 <114329278+emeraldap10@users.noreply.github.com>
2022-09-25 14:18:56 +0700

    Create step 5-11 index.php

---
+++ step 5-11 index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP in PHP</title>
+
+    <?php include ("class_lib.php"); ?>
+
+</head>
+<body>
+    <?php
+        $stefan = new person();
+        $jimmy = new person();
+
+        $stefan->set_name("Stefan Mischook");
+        $jimmy->set_name("Nick Waddles");
+
+        echo "Stefan's full name: " . $stefan->get_name();
+        echo "Nick's full name: " . $jimmy->get_name();
+    ?>
+</body>
+</html>
